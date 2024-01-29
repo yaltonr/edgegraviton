zarf-dev/zarf

skeleton of generate command

src/cmd/generate.go
@@ -1,0 +1,108 @@
+package cmd
+
+import (
+	"errors"
+
+	"github.com/defenseunicorns/zarf/src/config"
+	"github.com/spf13/cobra"
+)
+
+var generateCmd = &cobra.Command{
+	Use:     "generate COMMAND",
+	Aliases: []string{"g"},
+	Short:   "Zarf package generation wizard and commands",
+}
+
+var generateWizardCmd = &cobra.Command{
+	Use:     "wizard",
+	Aliases: []string{"w"},
+	Short:   "Interactive wizard to assist with package creation",
+	Long: "Starts an interactive sessions with zarf where the user will be quizzed survey\n" +
+		"style to create a new zarf.yaml without needing prerequisite knowledge.",
+	RunE: func(cmd *cobra.Command, args []string) (error) {
+		err := errors.New("Unimplemented")
+		return err
+	},
+}
+
+var generatePackageCmd = &cobra.Command{
+	Use: "component",
+	Aliases: []string{"com"},
+	Short: "Create or modify a component",
+	RunE: func(cmd *cobra.Command, args []string) (error) {
+		err := errors.New("Unimplemented")
+		return err
+	},
+}
+
+var generateImageCmd = &cobra.Command{
+	Use: "image",
+	Aliases: []string{"img"},
+	Short: "Add or modify an image in the list",
+	RunE: func(cmd *cobra.Command, args []string) (error) {
+		err := errors.New("Unimplemented")
+		return err
+	},
+}
+
+var generateConstantCmd = &cobra.Command{
+	Use: "constant",
+	Aliases: []string{"con"},
+	Short: "Create or modify a constant",
+	RunE: func(cmd *cobra.Command, args []string) (error) {
+		err := errors.New("Unimplemented")
+		return err
+	},
+}
+
+var generateVariableCmd = &cobra.Command{
+	Use: "variable",
+	Aliases: []string{"con"},
+	Short: "Create or modify a variable",
+	RunE: func(cmd *cobra.Command, args []string) (error) {
+		err := errors.New("Unimplemented")
+		return err
+	},
+}
+
+func init() {
+	initViper()
+
+	rootCmd.AddCommand(generateCmd)
+	generateCmd.AddCommand(generateWizardCmd)
+	generateCmd.AddCommand(generatePackageCmd)
+	generateCmd.AddCommand(generateImageCmd)
+	generateCmd.AddCommand(generateConstantCmd)
+	generateCmd.AddCommand(generateVariableCmd)
+
+	bindGenerateFlags()
+	bindWizardFlags()
+	bindPackageFlags()
+	bindImageFlags()
+	bindConstantFlags()
+	bindVariableFlags()
+}
+
+func bindGenerateFlags() {
+	generateCmd.PersistentFlags().StringVarP(&config.GenerateOptions.FilePath, "yaml-path", "f", "", "Path to the zarf yaml to generate or modify")
+}
+
+func bindWizardFlags() {
+	generateWizardCmd.Flags()
+}
+
+func bindPackageFlags() {
+	generatePackageCmd.Flags()
+}
+
+func bindImageFlags() {
+	generateImageCmd.Flags()
+}
+
+func bindConstantFlags() {
+	generateConstantCmd.Flags()
+}
+
+func bindVariableFlags() {
+	generateVariableCmd.Flags()
+}

yaltonr/edgegraviton

src/cmd/generate.go
@@ -1,0 +1,109 @@
+package cmd
+
+import (
+	"errors"
+
+	"github.com/defenseunicorns/zarf/src/cmd/common"
+	"github.com/defenseunicorns/zarf/src/config"
+	"github.com/spf13/cobra"
+)
+
+var generateCmd = &cobra.Command{
+	Use:     "generate COMMAND",
+	Aliases: []string{"g"},
+	Short:   "Zarf package generation wizard and commands",
+}
+
+var generateWizardCmd = &cobra.Command{
+	Use:     "wizard",
+	Aliases: []string{"w"},
+	Short:   "Interactive wizard to assist with package creation",
+	Long: "Starts an interactive sessions with zarf where the user will be quizzed survey\n" +
+		"style to create a new zarf.yaml without needing prerequisite knowledge.",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		err := errors.New("Unimplemented")
+		return err
+	},
+}
+
+var generatePackageCmd = &cobra.Command{
+	Use:     "component",
+	Aliases: []string{"com"},
+	Short:   "Create or modify a component",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		err := errors.New("Unimplemented")
+		return err
+	},
+}
+
+var generateImageCmd = &cobra.Command{
+	Use:     "image",
+	Aliases: []string{"img"},
+	Short:   "Add or modify an image in the list",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		err := errors.New("Unimplemented")
+		return err
+	},
+}
+
+var generateConstantCmd = &cobra.Command{
+	Use:     "constant",
+	Aliases: []string{"con"},
+	Short:   "Create or modify a constant",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		err := errors.New("Unimplemented")
+		return err
+	},
+}
+
+var generateVariableCmd = &cobra.Command{
+	Use:     "variable",
+	Aliases: []string{"con"},
+	Short:   "Create or modify a variable",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		err := errors.New("Unimplemented")
+		return err
+	},
+}
+
+func init() {
+	common.InitViper()
+
+	rootCmd.AddCommand(generateCmd)
+	generateCmd.AddCommand(generateWizardCmd)
+	generateCmd.AddCommand(generatePackageCmd)
+	generateCmd.AddCommand(generateImageCmd)
+	generateCmd.AddCommand(generateConstantCmd)
+	generateCmd.AddCommand(generateVariableCmd)
+
+	bindGenerateFlags()
+	bindWizardFlags()
+	bindSelfPackageGenerateFlags()
+	bindImageFlags()
+	bindConstantFlags()
+	bindVariableFlags()
+}
+
+func bindGenerateFlags() {
+	generateCmd.PersistentFlags().StringVarP(&config.GenerateOptions.FilePath, "yaml-path", "f", "", "Path to the zarf yaml to generate or modify")
+}
+
+func bindWizardFlags() {
+	generateWizardCmd.Flags()
+}
+
+func bindSelfPackageGenerateFlags() {
+	generatePackageCmd.Flags()
+}
+
+func bindImageFlags() {
+	generateImageCmd.Flags()
+}
+
+func bindConstantFlags() {
+	generateConstantCmd.Flags()
+}
+
+func bindVariableFlags() {
+	generateVariableCmd.Flags()
+}