zarf-dev/zarf

minimal package creation and editing

src/cmd/generate.go
@@ -2,8 +2,12 @@ package cmd
 
 import (
 	"errors"
+	"os"
 
 	"github.com/defenseunicorns/zarf/src/config"
+	"github.com/defenseunicorns/zarf/src/internal/message"
+	"github.com/defenseunicorns/zarf/src/internal/utils"
+	"github.com/defenseunicorns/zarf/src/types"
 	"github.com/spf13/cobra"
 )
 
@@ -24,9 +28,55 @@ var generateWizardCmd = &cobra.Command{
 		return err
 	},
 }
-
 var generatePackageCmd = &cobra.Command{
-	Use: "component",
+	Use: "package PACKAGE_NAME",
+	Aliases: []string{"pkg"},
+	Short: "Create or modify a package",
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		var dest string
+		message.Info("hit1")
+		if config.GenerateOptions.FilePath == "" {
+			dest = "zarf.yaml"
+		} else {
+			dest = config.GenerateOptions.FilePath
+		}
+
+		_, err := os.Stat(dest)
+
+		packageToGenerate := types.ZarfPackage{}
+		message.Info("hit2")
+
+		if err == nil {
+			message.Info("hit3")
+		// Specified zarf file exists
+			err = utils.ReadYaml(dest, &packageToGenerate)
+			if err != nil {
+				message.Fatal(err, err.Error())
+			}
+
+			packageToGenerate.Metadata.Name = args[0]
+
+		} else if errors.Is(err, os.ErrNotExist) {
+		// Specified zarf file does not exist
+
+		packageToGenerate.Metadata.Name = args[0]
+		packageToGenerate.Kind = "ZarfPackageConfig"
+
+		} else {
+			message.Fatalf(err, "Unkown error when checking for specified zarf file: %s", err.Error())
+		}
+
+		err = utils.WriteYaml(dest, packageToGenerate, 0644)
+		if err != nil {
+			message.Fatal(err, err.Error())
+		}
+		
+	},
+}
+
+var generateComponentCmd = &cobra.Command{
+	Use: "component COMPONENT_NAME",
 	Aliases: []string{"com"},
 	Short: "Create or modify a component",
 	RunE: func(cmd *cobra.Command, args []string) (error) {
@@ -38,7 +88,7 @@ var generatePackageCmd = &cobra.Command{
 var generateImageCmd = &cobra.Command{
 	Use: "image",
 	Aliases: []string{"img"},
-	Short: "Add or modify an image in the list",
+	Short: "Create or modify an image",
 	RunE: func(cmd *cobra.Command, args []string) (error) {
 		err := errors.New("Unimplemented")
 		return err
@@ -71,6 +121,7 @@ func init() {
 	rootCmd.AddCommand(generateCmd)
 	generateCmd.AddCommand(generateWizardCmd)
 	generateCmd.AddCommand(generatePackageCmd)
+	generateCmd.AddCommand(generateComponentCmd)
 	generateCmd.AddCommand(generateImageCmd)
 	generateCmd.AddCommand(generateConstantCmd)
 	generateCmd.AddCommand(generateVariableCmd)
@@ -78,6 +129,7 @@ func init() {
 	bindGenerateFlags()
 	bindWizardFlags()
 	bindPackageFlags()
+	bindComponentFlags()
 	bindImageFlags()
 	bindConstantFlags()
 	bindVariableFlags()
@@ -93,6 +145,10 @@ func bindWizardFlags() {
 
 func bindPackageFlags() {
 	generatePackageCmd.Flags()
+}
+
+func bindComponentFlags() {
+	generateComponentCmd.Flags()
 }
 
 func bindImageFlags() {

yaltonr/edgegraviton

src/cmd/generate.go
@@ -2,9 +2,13 @@ package cmd
 
 import (
 	"errors"
+	"os"
 
 	"github.com/defenseunicorns/zarf/src/cmd/common"
 	"github.com/defenseunicorns/zarf/src/config"
+	"github.com/defenseunicorns/zarf/src/pkg/message"
+	"github.com/defenseunicorns/zarf/src/pkg/utils"
+	"github.com/defenseunicorns/zarf/src/types"
 	"github.com/spf13/cobra"
 )
 
@@ -25,9 +29,55 @@ var generateWizardCmd = &cobra.Command{
 		return err
 	},
 }
-
 var generatePackageCmd = &cobra.Command{
-	Use:     "component",
+	Use: "package PACKAGE_NAME",
+	Aliases: []string{"pkg"},
+	Short: "Create or modify a package",
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		var dest string
+		message.Info("hit1")
+		if config.GenerateOptions.FilePath == "" {
+			dest = "zarf.yaml"
+		} else {
+			dest = config.GenerateOptions.FilePath
+		}
+
+		_, err := os.Stat(dest)
+
+		packageToGenerate := types.ZarfPackage{}
+		message.Info("hit2")
+
+		if err == nil {
+			message.Info("hit3")
+		// Specified zarf file exists
+			err = utils.ReadYaml(dest, &packageToGenerate)
+			if err != nil {
+				message.Fatal(err, err.Error())
+			}
+
+			packageToGenerate.Metadata.Name = args[0]
+
+		} else if errors.Is(err, os.ErrNotExist) {
+		// Specified zarf file does not exist
+
+		packageToGenerate.Metadata.Name = args[0]
+		packageToGenerate.Kind = "ZarfPackageConfig"
+
+		} else {
+			message.Fatalf(err, "Unkown error when checking for specified zarf file: %s", err.Error())
+		}
+
+		err = utils.WriteYaml(dest, packageToGenerate, 0644)
+		if err != nil {
+			message.Fatal(err, err.Error())
+		}
+
+	},
+}
+
+var generateComponentCmd = &cobra.Command{
+	Use: "component COMPONENT_NAME",
 	Aliases: []string{"com"},
 	Short:   "Create or modify a component",
 	RunE: func(cmd *cobra.Command, args []string) error {
@@ -72,6 +122,7 @@ func init() {
 	rootCmd.AddCommand(generateCmd)
 	generateCmd.AddCommand(generateWizardCmd)
 	generateCmd.AddCommand(generatePackageCmd)
+	generateCmd.AddCommand(generateComponentCmd)
 	generateCmd.AddCommand(generateImageCmd)
 	generateCmd.AddCommand(generateConstantCmd)
 	generateCmd.AddCommand(generateVariableCmd)
@@ -79,6 +130,7 @@ func init() {
 	bindGenerateFlags()
 	bindWizardFlags()
 	bindSelfPackageGenerateFlags()
+	bindComponentFlags()
 	bindImageFlags()
 	bindConstantFlags()
 	bindVariableFlags()
@@ -94,6 +146,10 @@ func bindWizardFlags() {
 
 func bindSelfPackageGenerateFlags() {
 	generatePackageCmd.Flags()
+}
+
+func bindComponentFlags() {
+	generateComponentCmd.Flags()
 }
 
 func bindImageFlags() {