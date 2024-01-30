zarf-dev/zarf

improving zarf's decision making abilities

src/cmd/package.go
@@ -14,7 +14,6 @@ import (
 
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/defenseunicorns/zarf/src/config"
-	"github.com/defenseunicorns/zarf/src/internal/generator"
 	"github.com/defenseunicorns/zarf/src/internal/packager"
 	"github.com/defenseunicorns/zarf/src/internal/utils"
 	"github.com/mholt/archiver/v3"
@@ -73,15 +72,16 @@ var packageDeployCmd = &cobra.Command{
 }
 
 var packageGenerateCmd = &cobra.Command{
-	Use:     "generate NAME",
+	Use:     "generate NAME [--from data]...",
 	Aliases: []string{"g"},
-	Args:    cobra.MaximumNArgs(1),
-	Short:   "Use to generate either an example package or a package from a resource",
+	Args:    cobra.ExactArgs(1),
+	Short:   "Use to generate either an example package or a package from resources",
 	Run: func(cmd *cobra.Command, args []string) {
+		pkgName := args[0]
 		if cmd.Flags().Changed("from") {
-			switch expression {
-			case condition:
-				
+			for _, componentResource := range config.GenerateOptions.From {
+				result := packager.DeduceResourceType(componentResource)
+				message.Info(pkgName + "'s " + componentResource + " is " + result + ", probably...")
 			}
 		} else {
 			message.Fatal(errors.New("Unimplemented"), "Unimplemented")
@@ -254,7 +254,7 @@ func bindDeployFlags() {
 func bindPackageGenerateFlags() {
 	generateFlags := packageGenerateCmd.Flags()
 
-	generateFlags.StringVar(&config.GenerateOptions.From, "from", "", "The location of the resource to generate a package from")
+	generateFlags.StringArrayVar(&config.GenerateOptions.From, "from", []string{}, "The location of the resource to generate a package from")
 }
 
 func bindInspectFlags() {

src/internal/packager/generate.go
@@ -1,0 +1,115 @@
+package packager
+
+import (
+	"net/url"
+	"os"
+	"regexp"
+
+	"github.com/defenseunicorns/zarf/src/internal/message"
+	"github.com/defenseunicorns/zarf/src/internal/utils"
+	"helm.sh/helm/v3/pkg/chart/loader"
+)
+
+func getTopLevelFiles(path string) (topLevelFiles []string) {
+	dirContents, err := os.ReadDir(path)
+	if err != nil {
+		message.Fatal(err, "FS error")
+	}
+	for _, content := range dirContents {
+		if !content.IsDir() {
+			topLevelFiles = append(topLevelFiles, path + "/" + content.Name())
+		}
+	}
+	return topLevelFiles
+}
+
+func isDir(path string) bool {
+	pathData, err := os.Stat(path)
+	if err != nil {
+		message.Fatal(err, "FS error")
+	}
+	return pathData.IsDir()
+}
+
+func isLocalFiles(data string) bool {
+	return false
+}
+
+func isGitChart(data string) bool {
+	return false
+
+}
+
+func isHelmRepoChart(data string) bool {
+	return false
+}
+
+func isLocalChart(data string) bool {
+	_, err := loader.LoadDir(data)
+	return err == nil
+}
+
+func isManifests(data string) bool {
+	ManifestFiles = []string{}
+	isYaml := regexp.MustCompile(`.*\.yaml$`).MatchString
+	if isDir(data) {
+		topLevelFiles := getTopLevelFiles(data)
+		for _, topLevelFile := range topLevelFiles {
+			if isYaml(topLevelFile) {
+				ManifestFiles = append(ManifestFiles, topLevelFile)
+			}
+		}
+		if len(ManifestFiles) > 0 {
+			return true
+		} else {
+			return false
+		}
+	} else {
+		if isYaml(data) {
+			return true
+		} else {
+			return false
+		}
+	}
+}
+
+func isRemoteFile(data string) bool {
+	return false
+}
+
+func isUrl(data string) bool {
+	urlData, err := url.ParseRequestURI(data)
+	return err == nil && urlData.Host != ""
+}
+
+var ManifestFiles []string
+
+
+func DeduceResourceType(componentResource string) string {
+	if !utils.InvalidPath(componentResource) {
+		message.Info("Is path")
+		if isLocalChart(componentResource) {
+			return "localChart"
+		} else if isManifests(componentResource) {
+			return "manifests"
+		} else if isLocalFiles(componentResource) {
+			return "localFiles"
+		} else {
+			return "unknown path"
+		}
+	} else if isUrl(componentResource) {
+		message.Info("Is url")
+		if isGitChart(componentResource) {
+			return "gitChart"
+		} else if isHelmRepoChart(componentResource) {
+			return "helmRepoChart"
+		} else if isRemoteFile(componentResource) {
+			return "remoteFile"
+		} else {
+			return "unknown url"
+		}
+	} else {
+		return "unparsable"
+	}
+	
+}

yaltonr/edgegraviton

src/cmd/package.go
@@ -23,6 +23,7 @@ import (
 	"github.com/defenseunicorns/zarf/src/config"
 	"github.com/defenseunicorns/zarf/src/pkg/cluster"
 	"github.com/defenseunicorns/zarf/src/pkg/packager"
+	internalPackager "github.com/defenseunicorns/zarf/src/internal/packager"
 	"github.com/defenseunicorns/zarf/src/pkg/utils/helpers"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -114,16 +115,17 @@ var packageMirrorCmd = &cobra.Command{
 }
 
 var packageGenerateCmd = &cobra.Command{
-	Use:     "generate NAME",
+	Use:     "generate NAME [--from data]...",
 	Aliases: []string{"g"},
-	Args:    cobra.MaximumNArgs(1),
-	Short:   "Use to generate either an example package or a package from a resource",
+	Args:    cobra.ExactArgs(1),
+	Short:   "Use to generate either an example package or a package from resources",
 	Run: func(cmd *cobra.Command, args []string) {
+		pkgName := args[0]
 		if cmd.Flags().Changed("from") {
-			// switch expression {
-			// case condition:
-
-			// }
+			for _, componentResource := range config.GenerateOptions.From {
+				result := internalPackager.DeduceResourceType(componentResource)
+				message.Info(pkgName + "'s " + componentResource + " is " + result + ", probably...")
+			}
 		} else {
 			message.Fatal(errors.New("Unimplemented"), "Unimplemented")
 		}
@@ -439,7 +441,7 @@ func bindMirrorFlags(v *viper.Viper) {
 func bindPackageGenerateFlags() {
 	generateFlags := packageGenerateCmd.Flags()
 
-	generateFlags.StringVar(&config.GenerateOptions.From, "from", "", "The location of the resource to generate a package from")
+	generateFlags.StringArrayVar(&config.GenerateOptions.From, "from", []string{}, "The location of the resource to generate a package from")
 }
 
 func bindInspectFlags(_ *viper.Viper) {

src/internal/packager/generate.go
@@ -1,0 +1,115 @@
+package packager
+
+import (
+	"net/url"
+	"os"
+	"regexp"
+
+	"github.com/defenseunicorns/zarf/src/pkg/message"
+	"github.com/defenseunicorns/zarf/src/pkg/utils"
+	"helm.sh/helm/v3/pkg/chart/loader"
+)
+
+func getTopLevelFiles(path string) (topLevelFiles []string) {
+	dirContents, err := os.ReadDir(path)
+	if err != nil {
+		message.Fatal(err, "FS error")
+	}
+	for _, content := range dirContents {
+		if !content.IsDir() {
+			topLevelFiles = append(topLevelFiles, path + "/" + content.Name())
+		}
+	}
+	return topLevelFiles
+}
+
+func isDir(path string) bool {
+	pathData, err := os.Stat(path)
+	if err != nil {
+		message.Fatal(err, "FS error")
+	}
+	return pathData.IsDir()
+}
+
+func isLocalFiles(data string) bool {
+	return false
+}
+
+func isGitChart(data string) bool {
+	return false
+
+}
+
+func isHelmRepoChart(data string) bool {
+	return false
+}
+
+func isLocalChart(data string) bool {
+	_, err := loader.LoadDir(data)
+	return err == nil
+}
+
+func isManifests(data string) bool {
+	ManifestFiles = []string{}
+	isYaml := regexp.MustCompile(`.*\.yaml$`).MatchString
+	if isDir(data) {
+		topLevelFiles := getTopLevelFiles(data)
+		for _, topLevelFile := range topLevelFiles {
+			if isYaml(topLevelFile) {
+				ManifestFiles = append(ManifestFiles, topLevelFile)
+			}
+		}
+		if len(ManifestFiles) > 0 {
+			return true
+		} else {
+			return false
+		}
+	} else {
+		if isYaml(data) {
+			return true
+		} else {
+			return false
+		}
+	}
+}
+
+func isRemoteFile(data string) bool {
+	return false
+}
+
+func isUrl(data string) bool {
+	urlData, err := url.ParseRequestURI(data)
+	return err == nil && urlData.Host != ""
+}
+
+var ManifestFiles []string
+
+
+func DeduceResourceType(componentResource string) string {
+	if !utils.InvalidPath(componentResource) {
+		message.Info("Is path")
+		if isLocalChart(componentResource) {
+			return "localChart"
+		} else if isManifests(componentResource) {
+			return "manifests"
+		} else if isLocalFiles(componentResource) {
+			return "localFiles"
+		} else {
+			return "unknown path"
+		}
+	} else if isUrl(componentResource) {
+		message.Info("Is url")
+		if isGitChart(componentResource) {
+			return "gitChart"
+		} else if isHelmRepoChart(componentResource) {
+			return "helmRepoChart"
+		} else if isRemoteFile(componentResource) {
+			return "remoteFile"
+		} else {
+			return "unknown url"
+		}
+	} else {
+		return "unparsable"
+	}
+
+}